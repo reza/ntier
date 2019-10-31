9a3a17b8e970cc2a45c5ce1e2d941ea6c409125f
reza <reza@users.noreply.github.com>
2019-11-01 08:38:36 +1100

    init code to connect to mysql server

--- app/index.js
+++ app/index.js
@@ -1,7 +1,23 @@
 const express = require('express')
 const app = express()
 const port = 3000
+var mysql = require('mysql')
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'secret',
+  database: 'demo'
+})
 
 app.get('/', (req, res) => res.send('Hello World!'))
-
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+connection.connect()
+
+connection.query('SELECT 1 + 1 AS solution', function (err, rows, fields) {
+  if (err) throw err
+
+  console.log('The query result is: ', rows[0].solution)
+})
+
+connection.end()
