c3348137b15b9329883b628722b6cfe2dc6b2472
reza <reza@users.noreply.github.com>
2019-11-01 10:01:42 +1100

    query  db and fetch message

--- app/index.js
+++ app/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 var mysql = require('mysql')
-
+var motd = "Hello"
 var connection = mysql.createConnection({
   host:  '192.168.99.100',  //docker-machine for dev 'localhost',
   user: 'root',
@@ -10,15 +10,17 @@ var connection = mysql.createConnection({
   database: 'demo'
 })
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.get('/', function (req, res) {
+  console.log('Web request:' + req)
 
-connection.connect()
-
-connection.query('SELECT 1 + 1 AS solution', function (err, rows, fields) {
-  if (err) throw err
-
-  console.log('The query result is: ', rows[0].solution)
+  connection.query('SELECT * FROM motd', function (err, rows, fields) {
+    if (err) throw err
+    motd=rows[0].message
+    console.log('Mysql DB Query Result:', motd)
+  })
+  res.send(motd)
 })
 
-connection.end()
+app.listen(port, () => console.log('App listening on port:'+ port) )
+
+
